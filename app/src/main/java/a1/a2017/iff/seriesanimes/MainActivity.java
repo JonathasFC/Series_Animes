f1d612b53d091ea20c844d25d39aa6a1b691afa6
JonathasFC <jonathasfc15@gmail.com>
2017-07-09 19:44:56 -0300

    Criando Recycler View 'Series'

--- app/src/main/java/a1/a2017/iff/seriesanimes/MainActivity.java
+++ app/src/main/java/a1/a2017/iff/seriesanimes/MainActivity.java
@@ -1,13 +1,29 @@
 package a1.a2017.iff.seriesanimes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btn_series;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_series = (Button) findViewById(R.id.btn_series)
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == btn_series)
+        {
+            Intent it = new Intent (this,)
+        }
     }
 }
