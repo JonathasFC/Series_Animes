2abae0e4dc43511409aa7672dad06ddc9b92986e
JonathasFC <jonathasfc15@gmail.com>
2017-07-09 12:38:26 -0300

    Iniciando o projeto Séries&Animes

---
+++ app/src/main/java/a1/a2017/iff/seriesanimes/MainActivity.java
@@ -0,0 +1,13 @@
+package a1.a2017.iff.seriesanimes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
